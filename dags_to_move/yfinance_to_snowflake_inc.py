1fac8d445c83de7a10238087307424f22b43747c
Keeyong (Max) <keeyonghan@hotmail.com>
2025-02-09 16:14:34 -0800

    Update yfinance_to_snowflake_inc.py

--- dags_to_move/yfinance_to_snowflake_inc.py
+++ dags_to_move/yfinance_to_snowflake_inc.py
@@ -1,32 +1,42 @@
-# In Cloud Composer, add apache-airflow-providers-snowflake to PYPI Packages
 from airflow import DAG
 from airflow.models import Variable
 from airflow.decorators import task
 from airflow.operators.python import get_current_context
-from airflow.providers.snowflake.hooks.snowflake import SnowflakeHook
 
 from datetime import datetime, timedelta
 from helpers import util
 
 import logging
+import os
 import pandas as pd
 import yfinance as yf
 
 
-@task
-def extract(symbol, debug=True):
+def get_today_yesterday():
+    """
+    - 지금 실행 시간을 기준으로 어제 날짜를 계산
+    """
     today = datetime.today().strftime('%Y-%m-%d')  # 먼저 오늘 날짜를 계산
     yesterday = (datetime.today() - timedelta(days=1)).strftime('%Y-%m-%d')  # 어제 날짜를 계산
+
+    return today, yesterday
+
+
+@task
+def extract(symbol, debug=True):
+
+    (today, yesterday) = get_today_yesterday()
 
     # yesterday의 값을 읽어와서 data 데이터프레임에 저장
     if debug:
         print(yesterday, today)
     data = yf.download(symbol, start=yesterday, end=today)
+    # 'symbol' 컬럼을 추가하고 모든 행에 symbol 값 할당
+    data['symbol'] = symbol
 
     """
     data 데이터프레임 내용 정리
     """
-    data.reset_index(inplace=True, names=['Date'])
     data.columns = data.columns.droplevel(1) # symbol 하나만 다루기에 ticker 레벨 제거
     if debug:
         print(data.head())
@@ -36,7 +46,8 @@ def extract(symbol, debug=True):
     """
     tmp_dir = Variable.get("data_dir", "/tmp/")
     file_path = f"{tmp_dir}{symbol}_{yesterday}.csv"
-    data.to_csv(file_path, index=False)  # 데이터를 CSV로 저장
+    data.to_csv(file_path)  # 데이터를 CSV로 저장
+
     return file_path
 
 
@@ -44,10 +55,7 @@ def extract(symbol, debug=True):
 def load(symbol, schema, table):
     cur = util.return_snowflake_conn("snowflake_conn")
 
-    # 먼저 오늘 날짜를 계산
-    today = datetime.today().strftime('%Y-%m-%d')
-    # 어제 날짜를 계산
-    yesterday = (datetime.today() - timedelta(days=1)).strftime('%Y-%m-%d')
+    (today, yesterday) = get_today_yesterday()
 
     # extract에서 만들어진 파일 경로 생성
     tmp_dir = Variable.get("data_dir", "/tmp/")
@@ -68,6 +76,7 @@ def load(symbol, schema, table):
         # date_to_process 날짜가 휴일인 경우에는 아무런 레코드도 존재하지 않음
         df = pd.read_csv(file_path)
         for index, row in df.iterrows():
+            print(row)
             sql = f"""INSERT INTO {table} (date, open, close, high, low, volume, symbol) VALUES (
             '{row["Date"]}', {row['Open']}, {row['Close']}, {row['High']}, {row['Low']}, {row['Volume']}, '{symbol}')"""
             logging.info(sql)
@@ -77,6 +86,14 @@ def load(symbol, schema, table):
         cur.execute("ROLLBACK;")
         print(e)
         raise e
+    finally: 
+        # file_path에서 파일 이름만 추출
+        file_name = os.path.basename(file_path)
+        # 스테이지에 올린 파일을 삭제
+        table_stage = f"@%{table}"
+        cur.execute(f"REMOVE {table_stage}/{file_name}")
+        # 연결 닫기
+        cur.close()
 
 
 with DAG(
@@ -90,6 +107,6 @@ with DAG(
 
     schema = "raw_data"
     table = "stock_price"
-    symbol = "AAPL"
+    symbol = "NVDA"
 
     extract(symbol) >> load(symbol, schema, table)
