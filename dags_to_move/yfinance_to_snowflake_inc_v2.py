0bcfeadb5318ef43f73a9efa9dbee39c2d18cae2
Keeyong (Max) <keeyonghan@hotmail.com>
2025-02-10 11:37:40 -0800

    Update yfinance_to_snowflake_inc_v2.py

--- dags_to_move/yfinance_to_snowflake_inc_v2.py
+++ dags_to_move/yfinance_to_snowflake_inc_v2.py
@@ -6,6 +6,7 @@ from airflow.operators.python import get_current_context
 from datetime import datetime, timedelta
 from helpers import util
 
+import logging
 import os
 import pandas as pd
 import yfinance as yf
@@ -59,9 +60,15 @@ def load(symbol, schema, table):
     print("data_interval_end", context["data_interval_end"])
 
     try:
+        df = pd.read_csv(file_path)
+        if len(df) == 0:
+            logging.info("No record to process")
+            return
+
         cur.execute(f"USE SCHEMA {schema};")
         cur.execute(f"""CREATE TABLE IF NOT EXISTS {table} (
-            date date, open float, close float, high float, low float, volume int, symbol varchar 
+            date date, open float, close float, high float, low float, volume int, symbol varchar,
+            PRIMARY KEY (date, symbol)
         )""")
 
         cur.execute("BEGIN;")
@@ -69,7 +76,6 @@ def load(symbol, schema, table):
 
         # 루프를 돌기는 하지만 사실 하나의 레코드 혹은 레코드가 없는 것을 예상
         # date_to_process 날짜가 휴일인 경우에는 아무런 레코드도 존재하지 않음
-        df = pd.read_csv(file_path)
         for index, row in df.iterrows():
             sql = f"""INSERT INTO {table} (date, open, close, high, low, volume, symbol) VALUES (
             '{row["Date"]}', {row['Open']}, {row['Close']}, {row['High']}, {row['Low']}, {row['Volume']}, '{symbol}')"""
@@ -81,18 +87,13 @@ def load(symbol, schema, table):
         print(e)
         raise e
     finally:
-        # file_path에서 파일 이름만 추출
-        file_name = os.path.basename(file_path)
-        # 스테이지에 올린 파일을 삭제
-        table_stage = f"@%{table}"
-        cur.execute(f"REMOVE {table_stage}/{file_name}")
         # 연결 닫기
         cur.close()
 
 
 with DAG(
     dag_id='YfinanceToSnowflake_inc_v2',
-    description="Business Owner: xyz, Copy Apple stock info to Snowflake",
+    description="Business Owner: xyz, Copy Nvidia stock info to Snowflake",
     start_date=datetime(2025,1,14),
     catchup=False,
     tags=['ETL', 'incremental'],
