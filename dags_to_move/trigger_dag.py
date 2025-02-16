a5bb0c356d54dbf3ce7d2e0d624ba0e4ac948838
Keeyong (Max) <keeyonghan@hotmail.com>
2025-02-16 14:25:24 -0800

    Update trigger_dag.py

--- dags_to_move/trigger_dag.py
+++ dags_to_move/trigger_dag.py
@@ -2,18 +2,16 @@ from airflow import DAG
 from airflow.operators.trigger_dagrun import TriggerDagRunOperator
 from datetime import datetime
 
-dag = DAG(
-    dag_id='SourceDag',
+with DAG(
+    dag_id='TriggerDag',
     start_date=datetime(2025, 1, 19),
     catchup=False,
     schedule='@daily'
-)
-
-trigger_task = TriggerDagRunOperator(
-    task_id='trigger_task',
-    trigger_dag_id='TargetDag',
-    conf={'path': 'value1'},
-    logical_date='{{ ds }}',
-    reset_dag_run=True,
-    dag=dag
-)
+) as dag:
+    trigger_task = TriggerDagRunOperator(
+        task_id='trigger_task',
+        trigger_dag_id='TargetDag',
+        conf={'path': '/opt/ml/conf'},
+        logical_date='{{ ds }}',
+        reset_dag_run=True
+    )
